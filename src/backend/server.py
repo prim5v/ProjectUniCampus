ff05e78d0a719a00c2eea431da0020c4f084034a
Prim5v <emax27348@gmail.com>
2026-07-19 18:53:34 +0300

    sunday commit2

--- src/backend/server.py
+++ src/backend/server.py
@@ -37,7 +37,15 @@ def ratelimit_error(e):
     }), 429
 
 # start cron job
-start_scheduler()
+# 
+import os
+
+# Only start the scheduler if NOT running under PythonAnywhere's uWSGI web server
+if "UWSGI_ORIGINAL_PROC_NAME" not in os.environ:
+    start_scheduler()
+else:
+    print("Skipping background scheduler initialization in uWSGI worker process.")
+
 
 @app.route("/generate_rsa")
 def generate_rsa():
