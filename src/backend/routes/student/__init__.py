5ac00717a36d428f4a44ab12e14daab29264c892
Prim5v <emax27348@gmail.com>
2026-08-01 15:18:04 +0300

    few issues1

--- src/backend/routes/student/__init__.py
+++ src/backend/routes/student/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-student_bp = Blueprint("reader", __name__, url_prefix="/student")
+student_bp = Blueprint("student", __name__, url_prefix="/student")
 
 from . import getProfile
