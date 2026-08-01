d008b46698f3aedcd30147e7162ca1041ef22871
Prim5v <emax27348@gmail.com>
2026-08-01 15:38:01 +0300

    few issues2

--- src/backend/routes/auth/studentLogin.py
+++ src/backend/routes/auth/studentLogin.py
@@ -1,8 +1,9 @@
 
 from backend.routes.auth import auth_bp
 from flask import request
+from backend.modules.auth.studentLoginModule import student_login
 
 @auth_bp.route("/student/login", methods=['POST'])
 def login_student():
     data = request.get_json()
-    return 
+    return student_login(data)
