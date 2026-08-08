52fe35fa4f88ed0ea67073dd521fac59b6fdf2f6
Prim5v <emax27348@gmail.com>
2026-08-08 06:55:21 +0300

    backend3

--- src/backend/routes/auth/createCampusAccount.py
+++ src/backend/routes/auth/createCampusAccount.py
@@ -1,6 +1,6 @@
 from flask import request
 from backend.middleware.auth import require_auth, require_role
-from backend.modules.auth.createCampusAccountModule import create_campus_account
+from backend.modules.auth.createCampusAccountModule import create_campus
 from backend.routes.auth import auth_bp
 
 
@@ -9,7 +9,7 @@ from backend.routes.auth import auth_bp
 def create_account():
     print("🔥 ROUTE HIT")
     data = request.get_json()
-    response = create_campus_account(data)
+    response = create_campus(data)
     return response
 
 # http://127.0.0.1:5000/auth/clerk/sync
