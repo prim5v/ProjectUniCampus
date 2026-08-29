7de5476a5b5149b6f371d7027269b45009dd06fa
Prim5v <emax27348@gmail.com>
2026-08-29 13:23:47 +0300

    backend2

--- src/backend/routes/admin/creaateDigitalId.py
+++ src/backend/routes/admin/creaateDigitalId.py
@@ -1,7 +1,23 @@
+# from flask import request
+
+# from backend.routes.admin import admin_bp
+# from backend.modules.admin.creaateDigitalIdModule import create_digital_id
+# from backend.utils.limiter import limiter
+# from backend.middleware.auth import require_auth
+
+
+# @admin_bp.route("/create/digital/id", methods=["POST"])
+# @limiter.limit("20 per minute")
+# @require_auth
+# def create_id():
+#     data = request.get_json()
+#     return create_digital_id(data)
+
 from flask import request
 
 from backend.routes.admin import admin_bp
 from backend.modules.admin.creaateDigitalIdModule import create_digital_id
+
 from backend.utils.limiter import limiter
 from backend.middleware.auth import require_auth
 
@@ -10,5 +26,7 @@ from backend.middleware.auth import require_auth
 @limiter.limit("20 per minute")
 @require_auth
 def create_id():
-    data = request.get_json()
+
+    data = request.form.to_dict()
+
     return create_digital_id(data)
