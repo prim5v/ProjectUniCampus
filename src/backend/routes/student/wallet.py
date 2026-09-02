24f8afc2a8ba8b100e739355155a470ca9f3fd01
Prim5v <emax27348@gmail.com>
2026-09-02 14:05:51 +0300

    backend2

--- src/backend/routes/student/wallet.py
+++ src/backend/routes/student/wallet.py
@@ -1,7 +1,6 @@
 from backend.routes.student import student_bp
 from backend.utils.jwt_setup import access_token_required
 from backend.modules.student.walletModule import wallet
-from flask import request, g
 from backend.utils.limiter import limiter
 
 
@@ -9,6 +8,5 @@ from backend.utils.limiter import limiter
 @limiter.limit("10 per minute")
 @access_token_required
 def my_wallet():
-    student_id = g.user_id
-    return wallet(student_id)
+    return wallet()
 
