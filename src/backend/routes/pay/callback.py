0f6c793609b6066b7dcc979ba8747cc2e6c24ec1
prim5v <emax27348@gmail.com>
2026-09-08 09:36:18 +0300

    backend

--- src/backend/routes/pay/callback.py
+++ src/backend/routes/pay/callback.py
@@ -6,7 +6,6 @@ from backend.utils.limiter import limiter
 
 @pay_bp.route("/student/deposit/callback", methods=["POST"])
 @limiter.limit("10 per minute")
-@access_token_required
 def mpesa_callback():
     payload = request.get_json()
     return stk_callback(payload)
