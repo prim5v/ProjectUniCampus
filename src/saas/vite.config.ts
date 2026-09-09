04977a0d18fb3379212f3fbfdfc36d818f2ce5bb
prim5v <emax27348@gmail.com>
2026-09-09 12:51:41 +0300

    saas2

--- src/saas/vite.config.ts
+++ src/saas/vite.config.ts
@@ -1,18 +1,18 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { vitePlugin as remix } from '@remix-run/dev' // 1. Import the Remix plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    remix(), // 2. Add the Remix plugin here
-    react()
-  ],
+  plugins: [react()],
 })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { vitePlugin as remix } from '@remix-run/dev' // 1. Import the Remix plugin
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     remix(), // 2. Add the Remix plugin here
+//     react()
+//   ],
+// })
